da93276e04bff1bca2f485b0715d7598f218d247
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-09-28 02:07:52 +0900

    spring-introduction-config-bean

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -1,6 +1,7 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,34 +9,34 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class MemberService {
 
-    @Autowired
-    public MemberService(MemoryMemberRepository memoryMemberRepository) {
-        this.memoryMemberRepository = memoryMemberRepository;
+public class MemberService {
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    private final MemoryMemberRepository memoryMemberRepository;
+    private final MemberRepository memberRepository;
+
+
 
     public Long join(Member member){
         vaildateDuplicateMember(member);
-        memoryMemberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void vaildateDuplicateMember(Member member) {
-        memoryMemberRepository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
         .ifPresent(member1 -> {
             throw new IllegalStateException("이미 존재하는 아이디 입니다");
         });
     }
     public List<Member> findMembers(){
-        return memoryMemberRepository.findall();
+        return memberRepository.findall();
     }
 
     public Optional<Member> findOne(Long id){
-        return memoryMemberRepository.findById(id);
+        return memberRepository.findById(id);
     }
 
 }
