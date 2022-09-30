cab53801b34756793828e8f37937a1e9019e7630
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-09-30 23:43:28 +0900

    spring-boot-mock-mvc-test

---
+++ book/src/main/java/com/jojoldu/book/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.jojoldu.book.controller;
+
+import com.jojoldu.book.web.HelloResponseDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(Model model){
+        return "hello";
+    }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name , @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
+    }
+}
---
+++ book/src/test/java/com/jojoldu/book/web/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.web;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 룸복_기능테스트(){
+        //given
+        String name = "test";
+        int amount =1000;
+
+        //when
+        HelloResponseDto result = new HelloResponseDto(name, amount);
+
+        //then
+        Assertions.assertThat(result.getName()).isEqualTo(name);
+        Assertions.assertThat(result.getAmount()).isEqualTo(amount);
+    }
+}
