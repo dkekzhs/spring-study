6b18be28f58cd0dadbee4b708609dc49288f723c
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-10-02 02:03:47 +0900

    spring-boot-google-session-login

--- book/src/main/java/com/jojoldu/book/controller/IndexController.java
+++ book/src/main/java/com/jojoldu/book/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.controller;
 
+import com.jojoldu.book.config.auth.SessionUser;
 import com.jojoldu.book.service.posts.PostsService;
 import com.jojoldu.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String Index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if( user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
