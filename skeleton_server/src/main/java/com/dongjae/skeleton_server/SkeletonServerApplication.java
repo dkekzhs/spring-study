978357c7bef5ba9294184623c60265f4cad6a9ae
dkekzhs <dkekzhs5@naver.com>
2024-11-30 23:49:08 +0900

    chore : init skeleton Server

---
+++ skeleton_server/src/main/java/com/dongjae/skeleton_server/SkeletonServerApplication.java
@@ -0,0 +1,13 @@
+package com.dongjae.skeleton_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeletonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeletonServerApplication.class, args);
+	}
+
+}
---
+++ skeleton_server/src/test/java/com/dongjae/skeleton_server/SkeletonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongjae.skeleton_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
